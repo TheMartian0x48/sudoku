e6b1061009a98b63bc21a5c9817c5787cbe5a4d6
Aditya Kumar Singh <cr7.aditya.cs@gmail.com>
2020-12-03 00:21:05 +0530

    Added cell.py

--- cell.py
+++ cell.py
@@ -2,21 +2,17 @@ import pygame
 
 
 class Cell:
-    def __init__(self, x, y, width, height, fixed):
+    def __init__(self, x, y, width, fixed, background, foreground):
         self.x = x
         self.y = y
         self.width = width
-        self.height = height
-        self.selected = False
         self.fixed = fixed
+        self.background = background
+        self.foreground = foreground
 
-    def draw(self, win, font, val):
-        pygame.draw.rect(win, (255, 255, 255), [
-                         self.x, self.y, self.width, self.height])
-        if val == -1:
+    def draw(self, window, font, val):
+        pygame.draw.rect(window, self.background, [self.x, self.y, self.width, self.width])
+        if val not in range(1, 10):
             return
-        color = (0, 255, 0)
-        if self.fixed == True:
-            color = (181, 154, 4)
-        text = font.render(str(val), True, color)
-        win.blit(text, (self.x + 20, self.y + 20))
+        text = font.render(str(val), True, self.foreground)
+        window.blit(text, (self.x + 20, self.y + 20))
